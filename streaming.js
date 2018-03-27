c906ceb7cfe440535da30d5d463514fc85636bf0
Juda Marto <juda.marto@stratus360.com>
2018-03-27 13:48:27 -0400

    Fixed the issue with duplicated subscriptions
    Only subscribe when there is no existing subscription by checking the node.subscriptionActive flag.

--- streaming.js
+++ streaming.js
@@ -1,5 +1,4 @@
-module.exports = function(RED) {
-    const nforce = require('./nforce_wrapper');
+const nforce = require('./nforce_wrapper');
 
     function Streaming(config) {
         const node = this;
@@ -12,56 +11,67 @@ module.exports = function(RED) {
 
         this.on('input', function(msg) {
             const action = msg.action || 'subscribe';
-            if (action === 'subscribe') {
-                // create connection object
-                node.status({ fill: 'blue', shape: 'ring', text: 'connecting....' });
-                debugger;
-                const org = nforce.createConnection(node.connection, msg);
-                nforce.authenticate(org, node.connection, msg, function(err, oauth) {
-                    if (err) {
-                        node.status({ fill: 'red', shape: 'dot', text: 'Error:' + err.message });
-                        return node.error(err, err.message);
-                    }
-                    const opts = {};
-                    const topicType = msg.topicType || config.topicType;
-                    if (topicType === 'platform') {
-                        opts.isEvent = true;
-                    } else if (topicType === 'generic') {
-                        opts.isSystem = true;
-                    }
-                    // Topic in message takes priority over configuration
-                    opts.topic = msg.topic || config.pushTopic;
-                    const subscriptionMessage = 'Subscription on ' + topicType + ' to:' + opts.topic;
-
-                    var stream;
-                    try {
-                        node.client = org.createStreamClient();
-                        stream = node.client.subscribe(opts);
-                        node.log(subscriptionMessage);
-                        node.status({ fill: 'blue', shape: 'dot', text: subscriptionMessage });
-                        node.subscriptionActive = true;
-                        stream.on('error', function(err) {
-                            node.log('Subscription error!!!');
+            if(!node.subscriptionActive){
+                if (action === 'subscribe') {
+                    // create connection object
+                    node.status({ fill: 'blue', shape: 'ring', text: 'connecting....' });
+                    debugger;
+                    const org = nforce.createConnection(node.connection, msg);
+                    nforce.authenticate(org, node.connection, msg, function(err, oauth) {
+                        let lastReplayId = -1;
+                        if (err) {
                             node.status({ fill: 'red', shape: 'dot', text: 'Error:' + err.message });
-                            node.log(err, msg);
-                            node.client.disconnect();
-                            node.subscriptionActive = false;
                             return node.error(err, err.message);
+                        }
+                        const opts = {};
+                        const topicType = msg.topicType || config.topicType;
+                        if (topicType === 'platform') {
+                            opts.isEvent = true;
+                        } else if (topicType === 'generic') {
+                            opts.isSystem = true;
+                        }
+                        // Topic in message takes priority over configuration
+                        opts.topic = msg.topic || config.pushTopic;
+                        const subscriptionMessage = 'Subscription on ' + topicType + ' to:' + opts.topic;
+    
+                        var stream;
+                        try {
+                            node.client = org.createStreamClient();
+                            stream = node.client.subscribe(opts);
+                            node.log(subscriptionMessage);
+                            node.status({ fill: 'blue', shape: 'dot', text: subscriptionMessage });
+                            node.subscriptionActive = true;
+                            stream.on('error', function(err) {
+                                node.log('Subscription error!!!');
+                                node.status({ fill: 'red', shape: 'dot', text: 'Error:' + err.message });
+                                node.log(err, msg);
+                                node.client.disconnect();
+                                node.subscriptionActive = false;
+                                return node.error(err, err.message);
+                            });
+                        } catch (ex) {
+                            node.status({ fill: 'red', shape: 'dot', text: 'Error:' + ex.message });
+                            return node.error(ex, ex.message);
+                        }
+    
+                        stream.on('data', function(data) {
+                            
+                            if(lastReplayId == data.event.replayId){
+                                return;
+                            }
+    
+                            lastReplayId = data.event.replayId;
+                            
+                            node.status({ fill: 'green', shape: 'dot', text: 'Receiving data' });
+                            node.send({
+                                payload: data
+                            });
+                            node.status({ fill: 'blue', shape: 'dot', text: subscriptionMessage });
                         });
-                    } catch (ex) {
-                        node.status({ fill: 'red', shape: 'dot', text: 'Error:' + ex.message });
-                        return node.error(ex, ex.message);
-                    }
-
-                    stream.on('data', function(data) {
-                        node.status({ fill: 'green', shape: 'dot', text: 'Receiving data' });
-                        node.send({
-                            payload: data
-                        });
-                        node.status({ fill: 'blue', shape: 'dot', text: subscriptionMessage });
                     });
-                });
-            } else if (node.subscriptionActive) {
+                }
+
+            } else {
                 // Unsubscribe only for active subscriptions
                 if (node.client.disconnect) {
                     node.client.disconnect();
